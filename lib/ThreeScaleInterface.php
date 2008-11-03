480b923cd13b3a8a01d34a55d3b72a47d809e537
adam <adam@3scale.net>
2008-11-03 19:12:53 +0100

    adds error handling code

--- lib/ThreeScaleInterface.php
+++ lib/ThreeScaleInterface.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once(dirname(__FILE__) . '/curl/curl.php');
+
 /**
  * Interface for communication with 3scale backend server.
  *
@@ -8,6 +10,7 @@ class ThreeScaleInterface {
 
   private $host;
   private $providerPrivateKey;
+  private $http;
 
   const KEY_PREFIX = '3scale-';
 
@@ -16,10 +19,13 @@ class ThreeScaleInterface {
    *
    * @param $host Hostname of 3scale backend server.
    * @param $providerPrivateKey Unique key that identifies this provider.
+   * @param $http Object for handling HTTP requests. Leaving it NULL will use
+   *              CURL by default, which should be fine for most cases.
    */
-  public function __construct($host = NULL, $providerPrivateKey = NULL) {
+  public function __construct($host = NULL, $providerPrivateKey = NULL, $http = NULL) {
     $this->host = $host;
     $this->providerPrivateKey = $providerPrivateKey;
+    $this->http = is_null($http) ? new Curl : $http;
   }
 
   /**
@@ -56,12 +62,42 @@ class ThreeScaleInterface {
 
   /**
    * Start a transaction.
-   * 
+   *
    * @param string $userKey
    * @param array $usage
    */
   public function start($userKey, $usage = array()) {
+    $url = "{$this->host}/transactions.xml";
+    $params = array(
+        'user_key' => $userKey,
+        'provider_key' => $this->getProviderPrivateKey(),
+        'usage' => $usage);
 
+    $response = $this->http->post($url, $params);
+
+    if ($response->headers['Status-Code'] == 200) {
+
+    } else {
+      $this->handleError($response->body);
+    }
+
+
+    //      params = {
+    //        'user_key' => prepare_key(user_key),
+    //        'provider_key' => provider_private_key
+    //      }
+    //      params.merge!(encode_params(usage, 'usage'))
+    //      response = Net::HTTP.post_form(uri, params)
+    //
+    //      if response.is_a?(Net::HTTPSuccess)
+    //        element = Hpricot::XML(response.body).at('transaction')
+    //        [:id, :provider_verification_key, :contract_name].inject({}) do |memo, key|
+    //          memo[key] = element.at(key).inner_text if element.at(key)
+    //          memo
+    //        end
+    //      else
+    //        handle_error(response.body)
+    //      end
   }
 
   /**
@@ -90,6 +126,33 @@ class ThreeScaleInterface {
   public function isSystemKey($key) {
     return strpos($key, self::KEY_PREFIX) === 0;
   }
+
+  private function handleError($body) {
+    static $codes_to_exceptions = array(
+    'user.exceeded_limits' => 'LimitsExceeded',
+    'user.invalid_key' => 'UserKeyInvalid',
+    'user.inactive_contract' => 'ContractNotActive',
+    'provider.invalid_key' => 'ProviderKeyInvalid',
+    'provider.invalid_metric' => 'MetricInvalid',
+    'provider.invalid_transaction_id' => 'TransactionNotFound');
+    
+    $xml = new SimpleXMLElement($body);
+    $exception_class = $codes_to_exceptions[(string) $xml['id']];
+
+    if ($exception_class) {
+      $exception_class = "ThreeScale{$exception_class}";
+      throw new $exception_class;
+    } else {
+      throw new ThreeScaleUnknownError;
+    }
+  }
 }
+
+class ThreeScaleException extends RuntimeException {}
+class ThreeScaleUserException extends ThreeScaleException {}
+class ThreeScaleUserKeyInvalid extends ThreeScaleUserException {}
+
+class ThreeScaleProviderException extends ThreeScaleException {}
+class ThreeScaleProviderKeyInvalid extends ThreeScaleProviderException {}
 
 ?>
--- test/ThreeScaleInterfaceTest.php
+++ test/ThreeScaleInterfaceTest.php
@@ -1,20 +1,60 @@
 <?php
 
-require_once(dirname(__FILE__) . '/simpletest/unit_tester.php');
-require_once(dirname(__FILE__) . '/simpletest/autorun.php');
+require_once('simpletest/unit_tester.php');
+require_once('simpletest/autorun.php');
 
 require_once(dirname(__FILE__) . '/../lib/ThreeScaleInterface.php');
 
+Mock::generate('Curl');
+
+class StubResponse {
+  public $body = '';
+  public $headers = array();
+}
+
 class ThreeScaleInterfaceTest extends UnitTestCase {
   private $interface;
+  private $http;
 
   function setUp() {
-    $this->interface = new ThreeScaleInterface('http://3scale.net', 'some_key');
+    $this->http = new MockCurl($this);
+    $this->interface = new ThreeScaleInterface('http://3scale.net', 'some_key',
+      $this->http);
   }
+
+  function testStartShouldThrowExceptionOnInvalidUserKey() {
+    $this->http->expectOnce('post',
+      array('http://3scale.net/transactions.xml', '*'));
+    $this->http->setReturnValue('post',
+      $this->stubError(403, 'user.invalid_key'));
+
+    $this->expectException(new ThreeScaleUserKeyInvalid);
+    $this->interface->start('invalid_key');
+  }
+
+  function testStartShouldThrowExceptionOnInvalidProviderKey() {
+    $this->http->expectOnce('post',
+      array('http://3scale.net/transactions.xml', '*'));
+    $this->http->setReturnValue('post',
+      $this->stubError(400, 'provider.invalid_key'));
+
+    $this->expectException(new ThreeScaleProviderKeyInvalid);
+    $this->interface->start('valid_key');
+  }
+
 
   function testShouldIdentify3scaleKeys() {
     $this->assertTrue($this->interface->isSystemKey('3scale-foo'));
     $this->assertFalse($this->interface->isSystemKey('foo'));
+  }
+
+  // Stub error response.
+  private function stubError($http_code, $error_code) {
+    $response = new StubResponse;
+    $response->headers['Status-Code'] = $http_code;
+    $response->body = "<error id=\"{$error_code}\">blah blah</error>";
+
+    return $response;
   }
 
 }
