f36d1db0a754d2e8ba37e145ff73540dcc7159ab
3scale SF <3scalesf@3scales-MacBook-Air.local>
2017-02-01 19:16:15 -0800

    fix failing test cases

--- test/RemoteTest.php
+++ test/RemoteTest.php
@@ -2,7 +2,8 @@
 
 if (getenv('TEST_3SCALE_PROVIDER_KEY') && 
     getenv('TEST_3SCALE_APP_IDS')      &&
-    getenv('TEST_3SCALE_APP_KEYS')) {
+    getenv('TEST_3SCALE_APP_KEYS')  &&
+    getenv('TEST_3SCALE_SERVICE_ID')) {
   error_reporting(E_ALL & ~E_DEPRECATED);
   require_once(dirname(__FILE__) . '/../lib/simpletest/unit_tester.php');
   require_once(dirname(__FILE__) . '/../lib/simpletest/autorun.php');
@@ -22,18 +23,21 @@ if (getenv('TEST_3SCALE_PROVIDER_KEY') &&
       $this->appKeys = explode(',', getenv('TEST_3SCALE_APP_KEYS'));
       $this->appKeys = array_map('trim', $this->appKeys);
 
+      $this->serviceId = explode(',', getenv('TEST_3SCALE_SERVICE_ID')); 
+      $this->serviceId = array_map('trim', $this->serviceId);
+
       $this->client = new ThreeScaleClient($this->providerKey);
     }
 
     function testSuccessfulAuthorize() {
       foreach($this->appKeys as $appKey) {
-        $response = $this->client->authorize($this->appIds[0], $appKey);
+        $response = $this->client->authorize($this->appIds[0], $appKey, $this->serviceId[0]);
         $this->assertTrue($response->isSuccess());
       }
     }
 
     function testFailedAuthorize() {
-      $response = $this->client->authorize('boo');
+      $response = $this->client->authorize('boo', $this->serviceId[0]);
       $this->assertFalse($response->isSuccess());
 
       $this->assertEqual('application_not_found', $response->getErrorCode());
@@ -43,11 +47,12 @@ if (getenv('TEST_3SCALE_PROVIDER_KEY') &&
 
     function testSuccessfulReport() {
       $transactions = array();
+
       foreach ($this->appIds as $appId) {
         array_push($transactions, array('app_id' => $appId, 'usage' => array('hits' => 1)));
       }
 
-      $response = $this->client->report($transactions);
+      $response = $this->client->report($transactions, $this->serviceId[0]);
       $this->assertTrue($response->isSuccess());
     }
 
@@ -58,7 +63,7 @@ if (getenv('TEST_3SCALE_PROVIDER_KEY') &&
       }
 
       $client = new ThreeScaleClient('boo');
-      $response = $client->report($transactions);
+      $response = $client->report($transactions, $this->serviceId[0]);
       
       $this->assertFalse($response->isSuccess());
       $this->assertEqual('provider_key_invalid', $response->getErrorCode());
@@ -68,8 +73,9 @@ if (getenv('TEST_3SCALE_PROVIDER_KEY') &&
   }
 
 } else {
-  echo "This test executes real requests against 3scale backend server. It needs to know provider key, application ids and application keys to use in the requests. You have to set these environment variables:\n";
-  echo " * TEST_3SCALE_PROVIDER_KEY - a provider key.\n";
+  echo "This test executes real requests against 3scale backend server. It needs to know provider key, service id, application ids and application keys to use in the requests. You have to set these environment variables:\n";
+  echo " * TEST_3SCALE_PROVIDER_KEY - provider key.\n";
+  echo " * TEST_3SCALE_SERVICE_ID   - service id.\n";
   echo " * TEST_3SCALE_APP_IDS      - list of application ids, separated by commas.\n";
   echo " * TEST_3SCALE_APP_KEYS     - list of application keys corresponding to the FIRST id in the TEST_3SCALE_APP_IDS list. Also separated by commas.\n";
 }
